7a39c1d87b101ca3e568f7558805281a0ef14c90
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2017-10-13 08:20:43 +0700

    add func

--- hello.go
+++ hello.go
@@ -2,11 +2,8 @@ package main
 
 import(
 	"fmt"
-	"github.com/facebookgo/clock"
 )
 
-func main(){
-	cl := clock.NewMock()
-	
-	fmt.Println("hello world",cl.Now())
+func PrintSometing(){
+	fmt.Println("hello bitches")
 }
